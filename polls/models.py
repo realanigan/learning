e924f01c321e8fa478d71a23c108782a09b0f783
Ree Jay <real.anigan@gmail.com>
2025-01-21 16:24:21 +0800

    finished basic tutorial

--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,18 @@ import datetime
 
 from django.utils import timezone
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    @admin.display(
+            boolean=True,
+            ordering="pub_date",
+            description="Publushed recently?",
+        )
 
     def was_published_recently(self):
         now = timezone.now()
